8ef683906859ab0530b3e4a4a88ed1c696c4dac1
Alan Fernandes <alanferns19@gmail.com>
2024-09-12 11:15:00 +0530

    updated github url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='A module for data visualization.',
     author='Alan Fernandes',
     author_email='alanferns19@gmail.com',
-    url='https://github.com/yourusername/VizEnhancer',  # Update with your repo URL
+    url='https://github.com/AlanFernandes8/Vizit',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
