92fa8ce26bee608dd9c005d18163b6fef2f3e7a8
Alan Fernandes <alanferns19@gmail.com>
2024-09-12 16:23:15 +0530

    URL update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='A module for data visualization.',
     author='Alan Fernandes',
     author_email='alanferns19@gmail.com',
-    url='https://github.com/AlanFernandes8/Vizit',
+    url='https://github.com/AlanFernandes8/vzn',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
